9bb466845b5451baacb11ee007a32ef1020ccb9b
Jacques Koorts <jkoorts@gmail.com>
2019-03-14 15:59:24 +0200

    Add composition tests

---
+++ src/main/java/com/example/demo/NameWrapper.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class NameWrapper {
+
+    private Name name;
+    private Surname surname;
+
+    public NameWrapper(String name, String surname) {
+        this.name = new Name();
+        this.name.setValue(name);
+
+        this.surname = new Surname();
+        this.surname.setValue(surname);
+    }
+
+    public String getName() {
+        return name.getValue();
+    }
+
+    public String getSurname() {
+        return surname.getValue();
+    }
+}
--- src/test/java/com/example/demo/ooprinciples/AlternativeInheritanceTests.java
+++ src/test/java/com/example/demo/ooprinciples/AlternativeInheritanceTests.java
@@ -1,8 +1,17 @@
 package com.example.demo.ooprinciples;
 
 import com.example.demo.DemoApplicationTests;
+import com.example.demo.NameWrapper;
+import org.junit.Test;
 
 public class AlternativeInheritanceTests extends DemoApplicationTests {
 
+    @Test
+    public void testAlternativeInheritance() {
+        NameWrapper nameWrapper = new NameWrapper("susan", "somesurname");
 
+        assert nameWrapper.getName().equals("susan");
+        assert nameWrapper.getSurname().equals("somesurname");
+
+    }
 }
